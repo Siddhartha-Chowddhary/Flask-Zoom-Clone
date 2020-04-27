859efcb05231cb30306688306f36cef88caea566
Siddhartha-Chowddhary <sangeeta11.chowddhary@gmail.com>
2020-04-27 11:14:50 +0530

    Update Routing.py

--- V2/Experiemnt3/Routing.py
+++ V2/Experiemnt3/Routing.py
@@ -16,7 +16,7 @@ def index():
     # form = LoginForms()
     # username = current_user.Username
     # print(username)
-    return render_template('index.html') 
+    return render_template('index.html')
 
 @app.route('/index2')
 def index2():
@@ -25,7 +25,7 @@ def index2():
     # username = current_user.Username
     # print(username)
 
-    return render_template('index.html')    
+    return render_template('index.html')
 
 @app.route('/CHAT/<CURRENT_ROOM>')
 def CHAT(CURRENT_ROOM):
@@ -34,30 +34,30 @@ def CHAT(CURRENT_ROOM):
     print(current_user.Username)
     text_file = open(f"E:/Zeus/Video_Call/InterWeb/Experiemnt3/static/Rooms/{current_user.Username}/Room.txt", "w")
     n = text_file.write(current_rooms)
-    text_file.close() 
+    text_file.close()
 
     text_file2 = open(f"E:/Zeus/Video_Call/InterWeb/Experiemnt3/static/Rooms/{current_user.Username}/Room.txt", "r")
 
     ROOM_NAME = text_file2.read()
     print(ROOM_NAME)
 
- 
 
 
-    return render_template('CHAT.html', USERS = current_user.Username, rooms=ROOM_NAME)  
+
+    return render_template('CHAT.html', USERS = current_user.Username, rooms=ROOM_NAME)
 
 @socketio.on('message')
 def message(data):
     print(f"\n\n{data}\n\n")
     send({'msg': data['msg'], 'username':data['username'],'time_stamp':
     strftime('%b-%d %I:%M%p', localtime())}, room=data['room'])
-    
+
 
 @socketio.on('join')
 def join(data):
-    
+
     join_room(data['room'])
-    send({'msg': data['username'] + "Has joined the" + 
+    send({'msg': data['username'] + "Has joined the" +
         data['room'] + "room."}, room=data['room'])
 
 
@@ -66,7 +66,7 @@ def join(data):
 def leave(data):
     print(f"\n\n{data}\n\n")
     leave_room(data['room'])
-    send({'msg': data['username'] + "Has left the" + 
+    send({'msg': data['username'] + "Has left the" +
         data['room'] + "room."},  room=data['room'])
 
 
@@ -81,30 +81,30 @@ def video_chat(CURRENT_ROOM):
     print(current_user.Username)
     text_file = open(f"E:/Zeus/Video_Call/InterWeb/Experiemnt3/static/Rooms/{current_user.Username}/videoRoom.txt", "w")
     n = text_file.write(current_rooms)
-    text_file.close() 
+    text_file.close()
 
     text_file2 = open(f"E:/Zeus/Video_Call/InterWeb/Experiemnt3/static/Rooms/{current_user.Username}/videoRoom.txt", "r")
 
     ROOM_NAME = text_file2.read()
     print(ROOM_NAME)
 
-    return render_template('Video_Chat.html',  USERS = current_user.Username, rooms=ROOM_NAME)    
+    return render_template('Video_Chat.html',  USERS = current_user.Username, rooms=ROOM_NAME)
 
 
-    
+
 @socketio.on('Video')
 def Video(data):
-    
+
     # print(f"\n\n{data}\n\n")
-    emit('RoomVideo', { 'videos': data['Image']})
+    emit('RoomVideo', { 'videos': data['Image']}, room=data['room'])
 
 
 
 @socketio.on('joinVideo')
 def joinVideo(data):
-    
+
     join_room(data['room'])
-    emit('RoomVideostatus',{'msg': data['username'] + "Has joined the" + 
+    emit('RoomVideostatus',{'msg': data['username'] + "Has joined the" +
         data['room'] + "room."}, room=data['room'])
 
 
@@ -113,6 +113,5 @@ def joinVideo(data):
 def leaveVideo(data):
     print(f"\n\n{data}\n\n")
     leave_room(data['room'])
-    emit('RoomVideostatus', {'msg': data['username'] + "Has left the" + 
+    emit('RoomVideostatus', {'msg': data['username'] + "Has left the" +
         data['room'] + "room."},  room=data['room'])
-
